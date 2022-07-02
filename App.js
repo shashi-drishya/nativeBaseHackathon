eda7ad06e96f0ed15d3afa9e5852edd6ca4b98bc
shashi <shashi.tiwari@drishya.ai>
2022-07-02 16:17:56 +0530

    minor updates

---
+++ App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Box,
+} from "native-base";
+import NativeBaseIcon from "./components/NativeBaseIcon";
+import { Platform } from "react-native";
+import Test from "./components/Test";
+import DashBoard from "./components/DashBoard";
+import Dashboard from "./components/DashBoard";
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Center
+        _dark={{ bg: "blueGray.900" }}
+        _light={{ bg: "blueGray.50" }}
+        px={4}
+        flex={1}
+      >
+        <Test />
+      </Center>
+    </NativeBaseProvider>
+  );
+}
+
+// Color Switch Component
+function ToggleDarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack space={2} alignItems="center">
+      <DashBoard />
+      <Text>Light</Text>
+      <Dashboard />
+    </HStack>
+  );
+}
---
+++ components/Test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Test() {
+  return <div>I am test anc</div>;
+}
+
+export default Test;
